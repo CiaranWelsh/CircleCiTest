8db1df10753542d016bb85eb6eaa451b106420d5
CiaranWelsh <cwelsh2@uw.edu>
2020-01-30 11:18:38 -0800

    s

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='mypkg',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='http://github.com/CiaranWelsh/CircleCiTest',
+      author='Ciaran Welsh',
+      author_email='cwelsh2@uw.edu',
+      license='MIT',
+      packages=['mypkg'],
+      zip_safe=False
+      )
+
+
+
